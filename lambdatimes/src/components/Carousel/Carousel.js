c7f2bed2b5482f866125c2248a23e3eb37849f32
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-18 11:22:53 -0700

    Carousel component completed, implemented with infinite scroll

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,50 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      currImg: "",
+      currIdx: 0
     }
   }
   componentDidMount(){
-
+    this.setState({
+      currImg: carouselData[0],
+      currIdx: 0
+    });
   }
 
   leftClick = () => {
-
+    let currIdx = this.state.currIdx;
+    currIdx--;
+    if (currIdx < 0){
+      currIdx = carouselData.length - 1;
+    }
+    this.setState({
+      currImg: carouselData[currIdx],
+      currIdx: currIdx
+    });
   }
 
   rightClick = () => {
-
+    let currIdx = this.state.currIdx;
+    currIdx++;
+    if (currIdx === carouselData.length){
+      currIdx = 0;
+    }
+    this.setState({
+      currImg: carouselData[currIdx],
+      currIdx: currIdx
+    });
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.currImg} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
